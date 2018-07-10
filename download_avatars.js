ff871d7dba5fa0377ee839d834ab045e8bbcd8ab
HypnoMama <sarahhermsen@gmail.com>
2018-07-10 00:10:45 +0000

    added getRepoContributors function

--- download_avatars.js
+++ download_avatars.js
@@ -2,3 +2,11 @@ var request = require('request');
 
 console.log("Welcome to the GitHub Avatar Downloader!");
 
+function getRepoContributors(repoOwner, repoName, cb) {
+
+}
+
+getRepoContributors("jquery", 'jquery', function(err, result) {
+  console.error("Errors: ", err);
+  console.log("Result: ", result);
+})
